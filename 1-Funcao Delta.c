e113d196766e5217a54af549687af18e3ce5c953
mariaclaraduhau <135389180+mariaclaraduhau@users.noreply.github.com>
2023-11-24 11:10:10 -0300

    1-Funcao Delta.c

--- 1-Funcao Delta.c
+++ 1-Funcao Delta.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int maior(int a, int b, int c){
+int delta(int a, int b, int c){
     int resultado;
     resultado= (b*b)-4*a*c;
     printf("%d", resultado);
@@ -11,7 +11,7 @@ int main() {
 
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
-    maior(a, b, c);
+    delta(a, b, c);
 
     return 0;
 }
