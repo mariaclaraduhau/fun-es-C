42fdc58c00fc6e73c3daadbc6a1dc7d49615f7e5
mariaclaraduhau <135389180+mariaclaraduhau@users.noreply.github.com>
2023-11-24 11:19:59 -0300

    1-Funcao Delta.c

--- 1-Funcao Delta.c
+++ 1-Funcao Delta.c
@@ -3,18 +3,15 @@
 int delta(int a, int b, int c){
     int resultado;
     resultado= (b*b)-4*a*c;
-    printf("%d", resultado);
     return(resultado);
 }
 
 int main() {
 
-    int a, b, c;
+    int a, b, c, resultado;
     scanf("%d %d %d", &a, &b, &c);
-    delta(a, b, c);
+    resultado = delta(a, b, c);
+    printf("%d", resultado);
 
     return 0;
 }
-
-
- 
